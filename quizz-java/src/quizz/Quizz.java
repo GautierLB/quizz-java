2d25134a061aa94c38aac5bb6b4733ea158823cf
vincent.dondain@gmail.com <vincent.dondain@gmail.com@6f24446d-c1e2-23a4-8fb7-cff12a09670f>
2013-12-11 14:05:59 +0000

    TODODO

--- quizz-java/src/quizz/Quizz.java
+++ quizz-java/src/quizz/Quizz.java
@@ -16,7 +16,7 @@ public class Quizz {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODODO code application logic here
     }
     
 }
