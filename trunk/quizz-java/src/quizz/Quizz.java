6ce785257aa07e1fba676a9bd70fbfe9195e5f4e
gautier.lb@gmail.com <gautier.lb@gmail.com@6f24446d-c1e2-23a4-8fb7-cff12a09670f>
2013-12-19 11:27:16 +0000

    - Amélioration de la vue createQuizz - Ajout de l'userIcon - Fix du bug police

--- trunk/quizz-java/src/quizz/Quizz.java
+++ trunk/quizz-java/src/quizz/Quizz.java
@@ -23,7 +23,8 @@ public class Quizz {
     /**
      * @param args the command line arguments
      */
-    public static Font s_openSans;
+    public static Font s_openSansTitle;
+    public static Font s_openSans13;
     
     public static void main(String[] args) {
         Quizz.initFont();
@@ -33,7 +34,8 @@ public class Quizz {
     
     public static void initFont(){
         try {
-          Quizz.s_openSans = new Font(Font.createFont(Font.TRUETYPE_FONT, new FileInputStream(new File("src/quizz/assets/OpenSans-Regular.ttf"))).getFamily(), 0, 34);
+          Quizz.s_openSansTitle = new Font(Font.createFont(Font.TRUETYPE_FONT, new FileInputStream(new File("src/quizz/assets/OpenSans-Regular.ttf"))).getFamily(), 0, 24);
+          Quizz.s_openSans13 = new Font(Font.createFont(Font.TRUETYPE_FONT, new FileInputStream(new File("src/quizz/assets/OpenSans-Regular.ttf"))).getFamily(), 0, 13);
         } catch (FontFormatException | IOException ex) {
             Logger.getLogger(Quizz.class.getName()).log(Level.SEVERE, null, ex);
             System.out.println("Erreur dans le chargement de la police");
