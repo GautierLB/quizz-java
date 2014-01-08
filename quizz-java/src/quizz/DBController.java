0d3f47e22d2ab163895385381d0faa9b18dc12f7
quentin.robert.epsi@gmail.com <quentin.robert.epsi@gmail.com@6f24446d-c1e2-23a4-8fb7-cff12a09670f>
2014-01-08 13:43:22 +0000

    Add class DBController.java with executeSelect

---
+++ quizz-java/src/quizz/DBController.java
@@ -0,0 +1,62 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quizz;
+
+import java.sql.*;
+
+public class DBController {
+
+    private Connection m_connection;
+    static DBController s_dbController;
+
+    static public DBController Get() {
+        if (s_dbController == null) {
+            s_dbController = new DBController();
+        }
+        return s_dbController;
+    }
+
+    private DBController() {
+        try {
+            // Load the SQLServerDriver class, build the
+            // connection string, and get a connection
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            String connectionUrl = "jdbc:sqlserver://193.252.48.189\\SQLEXPRESS:1433;"
+                    + "database=BDD_B3I_groupe_3;"
+                    + "user=b3i_groupe_3;"
+                    + "password=123Soleil";
+            this.m_connection = DriverManager.getConnection(connectionUrl);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("erreur connection");
+            System.exit(0);
+        }
+    }
+
+    /**
+     * Function how execute the specified sql query.
+     * <p>
+     * if the query failed return null
+     *
+     * @param select the data we want (for all use '*' )
+     * @param table the table of the data
+     * @return an ResultSet with the data
+     */
+    protected ResultSet executeSelect(String select, String table) {
+        ResultSet selectReturn;
+        try {
+            // Create and execute an SQL statement that returns some data. 
+            String SQL = "SELECT " + select + " FROM BDD_B3I_groupe_3.dbo.[" + table + "];";
+            Statement requête = this.m_connection.createStatement();
+            selectReturn = requête.executeQuery(SQL);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            selectReturn = null;
+        }
+        return selectReturn;
+    }
+    
+    
+}
