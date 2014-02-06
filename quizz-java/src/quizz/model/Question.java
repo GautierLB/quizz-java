8b620ad50769dc87fc5a1115130259d4a9a682e2
vincent.dondain@gmail.com <vincent.dondain@gmail.com@6f24446d-c1e2-23a4-8fb7-cff12a09670f>
2014-02-06 10:27:56 +0000

    === Brain Storming === #Fix - Fix DBController execute select with close mechanism.

--- quizz-java/src/quizz/model/Question.java
+++ quizz-java/src/quizz/model/Question.java
@@ -5,6 +5,7 @@ public class Question {
     private static DBController s_controller;
     private String m_labelQuestion;
     private String m_pictureQuestion;
+    private int m_id;
 
     /**
      * The construct of a Question
@@ -15,6 +16,7 @@ public class Question {
     public Question(String label, String picture) {
         this.m_labelQuestion = label;
         this.m_pictureQuestion = picture;
+        this.m_id = 0;
     }
 
     /**
@@ -23,8 +25,13 @@ public class Question {
      * @return the id of the created question
      */
     public int saveQuestionInDB() {
-        return this.s_controller.Get().executeInsert("questions",
+        this.m_id = this.s_controller.Get().executeInsert("questions",
                 "label_question,picture_question",
                 "'" + this.m_labelQuestion + "','" + this.m_pictureQuestion + "'");
+        return this.m_id;
+    }
+    
+    public int getId() {
+        return this.m_id;
     }
 }
