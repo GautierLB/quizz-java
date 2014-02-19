ed96b5f6d58631e9646aac501e80a75ba6d8c859
quentin.robert.epsi@gmail.com <quentin.robert.epsi@gmail.com@6f24446d-c1e2-23a4-8fb7-cff12a09670f>
2014-02-19 14:00:56 +0000

    add getAllThemes ==> return all the theme of the database add getThemeById ==> return the theme we want modification saveTheme , use the format Theme.NAME

--- trunk/quizz-java/src/quizz/model/Theme.java
+++ trunk/quizz-java/src/quizz/model/Theme.java
@@ -1,7 +1,14 @@
 package quizz.model;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Theme {
 
+    public static final String ID = "ID_THEME";
+    public static final String NAME = "NAME_THEME";
+    public static final String PICTURE = "PICTURE_THEME";
+    private int m_idTheme;
     private String m_nameTheme;
     private String m_pictureTheme;
 
@@ -11,9 +18,40 @@ public class Theme {
      * @param name the name of the Theme
      * @param picture the path or URL of the picture
      */
-    public Theme(String name, String picture) {
+    public Theme(int id, String name, String picture) {
+        this.m_idTheme = id;
         this.m_nameTheme = name;
         this.m_pictureTheme = picture;
+    }
+
+    /**
+     * Get all the Theme of the database
+     *
+     * @return an ArrayList of Theme
+     */
+    static public ArrayList<Theme> getAllThemes() {
+        ArrayList<Theme> themeList = new ArrayList<>();
+        ArrayList<HashMap<String, Object>> themeListBdd = DBController.Get().executeSelect("*", "theme", "");
+        for (int i = 0; i < themeListBdd.size(); i++) {
+            Theme theme = new Theme((int) themeListBdd.get(i).get(Theme.ID),
+                    (String) themeListBdd.get(i).get(Theme.NAME),
+                    (String) themeListBdd.get(i).get(Theme.PICTURE));
+            themeList.add(theme);
+        }
+        return themeList;
+    }
+
+    /**
+     * Get a theme by the id
+     *
+     * @param id the id of the theme we want
+     * @return the theme we want
+     */
+    static public Theme getThemeById(int id) {
+        ArrayList<HashMap<String, Object>> themeList = DBController.Get().executeSelect("*", "theme", "where ID_THEME = " + id);
+        return new Theme((int) themeList.get(0).get(Theme.ID),
+                (String) themeList.get(0).get(Theme.NAME),
+                (String) themeList.get(0).get(Theme.PICTURE));
     }
 
     /**
@@ -21,7 +59,7 @@ public class Theme {
      */
     public void saveTheme() {
         DBController.Get().executeInsert("theme",
-                "name_theme,picture_theme",
+                Theme.NAME + ',' + Theme.PICTURE,
                 "'" + this.m_nameTheme
                 + "','" + this.m_pictureTheme + "'");
     }
