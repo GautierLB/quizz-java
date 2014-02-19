baf84821456f3fc9a7aa8ddeb3aa0f63d008d33f
quentin.robert.epsi@gmail.com <quentin.robert.epsi@gmail.com@6f24446d-c1e2-23a4-8fb7-cff12a09670f>
2014-02-19 14:16:24 +0000

    add getScoreForQuizz ==> return all the score for one quizz modificate saveScoring with the format Scoring.SCORE add the final String QUIZZ , SCORE , USER

--- trunk/quizz-java/src/quizz/model/Scoring.java
+++ trunk/quizz-java/src/quizz/model/Scoring.java
@@ -1,7 +1,13 @@
 package quizz.model;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Scoring {
 
+    public static final String USER = "ID_USER";
+    public static final String QUIZZ = "ID_QUIZZ";
+    public static final String SCORE = "SCORE";
     private int m_idUser;
     private int m_idQuizz;
     private int m_score;
@@ -20,11 +26,29 @@ public class Scoring {
     }
 
     /**
+     * Get all the score for the quizz
+     *
+     * @param idQuizz the id of the quizz
+     * @return an ArrayList of scoring
+     */
+    static public ArrayList<Scoring> getScoreForQuiz(int idQuizz) {
+        ArrayList<Scoring> scoreList = new ArrayList<>();
+        ArrayList<HashMap<String, Object>> scoreListBdd = DBController.Get().executeSelect("*", "scoring", "WHERE ID_QUIZZ =" + idQuizz);
+        for (int i = 0; i < scoreListBdd.size(); i++) {
+            Scoring score = new Scoring((int) scoreListBdd.get(i).get(Scoring.USER),
+                    (int) scoreListBdd.get(i).get(Scoring.QUIZZ),
+                    (int) scoreListBdd.get(i).get(Scoring.SCORE));
+            scoreList.add(score);
+        }
+        return scoreList;
+    }
+
+    /**
      * Save in the database the score of the quiz
      */
     public void saveScoring() {
         DBController.Get().executeInsert("scoring",
-                "id_user,id_quizz,score",
+                Scoring.USER + ',' + Scoring.QUIZZ + ',' + Scoring.SCORE,
                 "" + this.m_idUser
                 + "," + this.m_idQuizz
                 + "," + this.m_score);
