f1e32fe2e673e5282ec1dadf880101d9f80821e9
gautier.lb@gmail.com <gautier.lb@gmail.com@6f24446d-c1e2-23a4-8fb7-cff12a09670f>
2014-03-26 14:23:37 +0000

    - Modification d'imagePanel

--- quizz-java/src/quizz/ImagePanel.java
+++ quizz-java/src/quizz/ImagePanel.java
@@ -30,8 +30,9 @@ public class ImagePanel extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents(Boolean response) {
 
-        addImageLabel = new javax.swing.JLabel();
+        urlLabel = new javax.swing.JLabel();
         titleLabel = new javax.swing.JLabel();
+        urlTextbox = new javax.swing.JTextField();
 
         setBackground(new java.awt.Color(255, 255, 255));
         setMaximumSize(new java.awt.Dimension(309, 307));
@@ -39,28 +40,23 @@ public class ImagePanel extends javax.swing.JPanel {
         setPreferredSize(new java.awt.Dimension(309, 307));
         setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        addImageLabel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/quizz/assets/plus-45.png"))); // NOI18N
-        addImageLabel.setMaximumSize(new java.awt.Dimension(45, 45));
-        addImageLabel.setMinimumSize(new java.awt.Dimension(45, 45));
-        addImageLabel.setPreferredSize(new java.awt.Dimension(45, 45));
-        add(addImageLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(132, 130, -1, -1));
+        urlLabel.setFont(Main.s_openSans13);
+        urlLabel.setText("Url de l'image");
+        add(urlLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 130, -1, -1));
 
         titleLabel.setFont(Main.s_openSans13);
         titleLabel.setForeground(new java.awt.Color(40, 40, 40));
-        titleLabel.setText("Ajoutez une Question");
+        titleLabel.setText("Ajoutez une Image");
         add(titleLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 40, -1, -1));
-		if( response == true){
+
+        if( response == true){
 			firstCircle = new javax.swing.JLabel();
 			secondCircle = new javax.swing.JLabel();
 			thirdCircle = new javax.swing.JLabel();
 			fourthCircle = new javax.swing.JLabel();
 			addResponse = new javax.swing.JLabel();
-			addResponse.addMouseListener(new java.awt.event.MouseAdapter() {
-				public void mouseClicked(java.awt.event.MouseEvent evt) {
-					addResponseMouseClicked(evt);
-				}
-			});
 			GoodAnswer = new javax.swing.JCheckBox();
+			
 			firstCircle.setIcon(new javax.swing.ImageIcon(getClass().getResource("/quizz/assets/circleEmpty-15.png"))); // NOI18N
 			add(firstCircle, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 280, -1, -1));
 
@@ -76,6 +72,7 @@ public class ImagePanel extends javax.swing.JPanel {
 			addResponse.setIcon(new javax.swing.ImageIcon(getClass().getResource("/quizz/assets/plus-20.png"))); // NOI18N
 			add(addResponse, new org.netbeans.lib.awtextra.AbsoluteConstraints(245, 250, -1, -1));
 			titleLabel.setText("Ajoutez une Réponse");
+			
 			GoodAnswer.setFont(Main.s_openSans13);
 			GoodAnswer.setLabel("Bonne réponse");
 			GoodAnswer.setMaximumSize(new java.awt.Dimension(150, 20));
@@ -85,7 +82,11 @@ public class ImagePanel extends javax.swing.JPanel {
 			add(GoodAnswer, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 250, -1, -1));
 		}
 
-        
+        urlTextbox.setText(" ");
+        urlTextbox.setMaximumSize(new java.awt.Dimension(80, 20));
+        urlTextbox.setMinimumSize(new java.awt.Dimension(80, 20));
+        urlTextbox.setPreferredSize(new java.awt.Dimension(80, 20));
+        add(urlTextbox, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 150, -1, -1));
     }// </editor-fold>//GEN-END:initComponents
 
     private void addResponseMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_addImageLabelMouseClicked
@@ -96,13 +97,14 @@ public class ImagePanel extends javax.swing.JPanel {
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel addImageLabel;
     private javax.swing.JLabel addResponse;
     private javax.swing.JLabel firstCircle;
     private javax.swing.JLabel fourthCircle;
     private javax.swing.JLabel secondCircle;
     private javax.swing.JLabel thirdCircle;
     private javax.swing.JLabel titleLabel;
-	private javax.swing.JCheckBox GoodAnswer;
+    private javax.swing.JLabel urlLabel;
+    private javax.swing.JTextField urlTextbox;
+	public javax.swing.JCheckBox GoodAnswer;
     // End of variables declaration//GEN-END:variables
 }
