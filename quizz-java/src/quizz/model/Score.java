c288ebe3bff306b7e2883883f7762cee6c1b9e9d
vincent.dondain@gmail.com <vincent.dondain@gmail.com@6f24446d-c1e2-23a4-8fb7-cff12a09670f>
2014-04-16 13:20:40 +0000

    === Brain Storming === #Fix - Corrections mineures score.

--- quizz-java/src/quizz/model/Score.java
+++ quizz-java/src/quizz/model/Score.java
@@ -10,7 +10,6 @@ public class Score {
     private static final String USER = "ID_USER";
     private static final String QUIZZ = "ID_QUIZZ";
     private static final String SCORE = "SCORE";
-    private int m_idUser;
     private int m_idQuizz;
     private int m_score;
     private int m_numberOfGoodAnswers;
@@ -20,13 +19,11 @@ public class Score {
     /**
      * The construct of Scoring
      *
-     * @param idUser the Id of the user how make the score
      * @param idQuizz the Id of the quizz how is concerne by the Score
      * @param score the score of the user
      */
-    public Score(int idUser, int idQuizz, int score) {
-        this.m_idQuizz = idUser;
-        this.m_idUser = idQuizz;
+    public Score(int idQuizz, int score) {
+        this.m_idQuizz = idQuizz;
         this.m_score = score;
     }
     
@@ -56,8 +53,7 @@ public class Score {
         ArrayList<Score> scoreList = new ArrayList<>();
         ArrayList<HashMap<String, Object>> scoreListBdd = DBController.Get().executeSelect("*", "scoring", "WHERE ID_QUIZZ =" + idQuizz);
         for (int i = 0; i < scoreListBdd.size(); i++) {
-            Score score = new Score((int) scoreListBdd.get(i).get(Score.USER),
-                    (int) scoreListBdd.get(i).get(Score.QUIZZ),
+            Score score = new Score((int) scoreListBdd.get(i).get(Score.QUIZZ),
                     (int) scoreListBdd.get(i).get(Score.SCORE));
             scoreList.add(score);
         }
