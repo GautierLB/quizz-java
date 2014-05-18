a2e0f6ac2381ed6730fd23110fe770c3ecb4fe84
gautier.lb@gmail.com <gautier.lb@gmail.com@6f24446d-c1e2-23a4-8fb7-cff12a09670f>
2014-05-18 11:30:30 +0000

    - Fix du timer qui s'arrête pas en quittant

--- quizz-java/src/quizz/QuestionScreenView.java
+++ quizz-java/src/quizz/QuestionScreenView.java
@@ -438,6 +438,7 @@ public class QuestionScreenView extends BrainStormingView {
     }//GEN-LAST:event_userLabelMouseClicked
 
     private void quitButtonAction(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_quitButtonAction
+        this.m_timer.cancel();
         m_mainFrame.changeView(MainFrame.View.MainScreenView);
     }//GEN-LAST:event_quitButtonAction
 
